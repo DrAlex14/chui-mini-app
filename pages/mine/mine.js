831ee7b3fd7c6b6372b6b0b8fd8125936af51c8d
zx-zzy <975722423@qq.com>
2022-08-09 17:49:54 +0800

    登录功能

--- pages/mine/mine.js
+++ pages/mine/mine.js
@@ -1,66 +1,49 @@
-// pages/mine.js
+// index.js
+
+// 获取应用实例
+const app = getApp()
+
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    userInfo: {},
+    openId: '',
+    needLogin: true
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  onLoad() {
+    const userInfo = wx.getStorageSync('userInfo');
+    if (userInfo) {
+      this.setData({
+        needLogin: false,
+        userInfo: userInfo
+      })
+    }
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  getUserProfile(e) {
+    // 推荐使用wx.getUserProfile获取用户信息，开发者每次通过该接口获取用户个人信息均需用户确认，开发者妥善保管用户快速填写的头像昵称，避免重复弹窗
+    wx.getUserProfile({
+      desc: '展示用户信息', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
+      success: (res) => {
+        console.log(res)
+        res.userInfo.openId = this.data.openId
+        this.setData({
+          userInfo: res.userInfo,
+          needLogin: false
+        })
+        wx.setStorageSync('userInfo', this.data.userInfo);
+      }
+    })
+    wx.cloud.callFunction({
+      name: 'login',
+      success: res => {
+        console.log('调用云函数成功');
+        console.log(res);
+        this.setData({
+          openId: res.result.openid
+        })
+      },
+      fail: err => {
+        console.log('调用失败'+ err);
+      }
+    })
   }
 })
