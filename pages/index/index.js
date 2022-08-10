a9adc57d97bbb038a2bc0be584cd1aa3160e288e
zx-zzy <975722423@qq.com>
2022-08-10 15:29:17 +0800

    重构代码，新增在一起天数功能

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,12 +1,28 @@
-// index.js
+import formatTime from '../../utils/util'
+
 // 获取应用实例
 const app = getApp()
 
 Page({
   data: {
-    
+    howLongDays: 0,
   },
   onLoad() {
 
+  },
+  onShow() {
+    this.calcHowLongDays();
+  },
+
+  // methods
+  
+  // 计算在一起时间
+  calcHowLongDays() {
+    let today = new Date();
+    let firstDay = Date.parse('2020-11-22');
+    let howLongDays = Math.floor(parseInt(today - firstDay) / (1000 * 60 * 60 * 24));
+    this.setData({
+      howLongDays: howLongDays
+    })
   }
 })
